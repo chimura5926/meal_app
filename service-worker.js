94b35e264792e60e4cb52564879387a2554e5ff1
chimura <yuki_chimura@icloud.com>
2026-03-13 22:55:22 +0900

    アプリ作成

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
-self.addEventListener("install",e=>{
-console.log("PWA installed");
+self.addEventListener("install", e => {
+    console.log("PWA installed");
+});
+
+// アプリとして認識されるための必須イベント（とりあえず通信を素通りさせる）
+self.addEventListener("fetch", e => {
+    // 現在はオフライン対応をしないので何もせずスルー
 });
